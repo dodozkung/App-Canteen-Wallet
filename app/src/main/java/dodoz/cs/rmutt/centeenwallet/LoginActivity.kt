4e2bd2c63df80f3e8623851aa0497124b6e2ff12
dodoz <dodozkung@gmail.com>
2020-10-20 03:16:25 +0700

    login verify

---
+++ app/src/main/java/dodoz/cs/rmutt/centeenwallet/LoginActivity.kt
@@ -0,0 +1,17 @@
+package dodoz.cs.rmutt.centeenwallet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+
+
+        }
+
+    }
--- app/src/main/java/dodoz/cs/rmutt/centeenwallet/SplashActivity.kt
+++ app/src/main/java/dodoz/cs/rmutt/centeenwallet/SplashActivity.kt
@@ -1,11 +1,26 @@
 package dodoz.cs.rmutt.centeenwallet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
+
+    private var handler : Handler? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            finish()
+        },4000)
+
     }
-}
+
+
+    }
