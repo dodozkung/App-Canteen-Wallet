f3cc337ee828469d4cc8caa09cae0eee60a4f853
dodoz <dodozkung@gmail.com>
2020-12-24 02:32:32 +0700

    api login+regis

---
+++ app/src/main/java/dodoz/cs/rmutt/canteenwallet/PinActivity.kt
@@ -0,0 +1,11 @@
+package dodoz.cs.rmutt.canteenwallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pin)
+    }
+}
